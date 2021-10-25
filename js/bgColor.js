adf77f93094161a3e491bce3f8be3f0b64c326c2
sahmed011 <90866414+sahmed011@users.noreply.github.com>
2021-10-24 20:13:29 -0400

    Latest Portfolio Changes
    Changes for the Web Design Capstone. Week 5 submission.

--- js/bgColor.js
+++
@@ -1,8 +0,0 @@
-function darkMode() {
-    document.body.style.backgroundColor = 'black';
-    document.body.style.color = 'white';
-}
-function lightMode() {
-    document.body.style.backgroundColor = 'white';
-    document.body.style.color = 'black';
-}
